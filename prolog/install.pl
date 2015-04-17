db50e70f6c7a1fb50e14c63c7ac85cab06d54993
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-04-16 19:48:47 -0700

    Adding install script for dependencies.  Changes to be committed: 	modified:   install.pl

--- prolog/install.pl
+++ prolog/install.pl
@@ -1,9 +1,34 @@
+
 :- module(install_cwt_prolog, []).
 /** <module> this module ensures required packs are loaded
  *
  */
 
-:- pack_install(lambda).
-:- pack_install(func).
 
+pack_deps([func, lambda]).
+
+
+main :-
+  search(Installs),
+  install_packs(Installs).
+
+
+list_packs(Packs) :-
+  findall(Pack, prolog_pack:current_pack(Pack), Packs).
+
+
+search(Installs) :-
+  list_packs(Packs),
+  pack_deps(Deps),
+  subtract(Deps, Packs, Installs).
+
+  
+install_packs([]) :-
+  writeln('Congratulations, all dependencies are met.').
+
+install_packs([P|_]) :-
+  pack_install(P),
+  search(Remaining),
+  install_packs(Remaining).
+  
 
